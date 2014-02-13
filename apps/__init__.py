04f89d8af77c464d7ebc2560a8a68319c9f4282b
Chi-En Wu <jason2506.mail@gmail.com>
2014-02-14 02:15:32 +0800

    Extract initialization functions

--- apps/__init__.py
+++ apps/__init__.py
@@ -9,6 +9,26 @@ from .views import module
 __all__ = ('create_app',)
 
 
+def _init_db(app, create_db):
+    db.app = app
+    db.init_app(app)
+    if create_db:
+        db.create_all()
+
+
+def _init_jinja(app):
+    app.jinja_env.globals['site_title'] = 'Skeleton'
+    app.jinja_env.globals['description'] = ''
+    app.jinja_env.globals['author'] = ''
+
+
+def _init_login(app):
+    login_manager = LoginManager()
+    login_manager.init_app(app)
+    login_manager.user_loader(User.get)
+    login_manager.login_view = '/login'
+
+
 def create_app(name=None, create_db=False):
     if name is None:
         name = __name__
@@ -16,15 +36,9 @@ def create_app(name=None, create_db=False):
     app = Flask(name)
     app.config.from_object('config')
 
-    db.app = app
-    db.init_app(app)
-    if create_db:
-        db.create_all()
-
-    login_manager = LoginManager()
-    login_manager.init_app(app)
-    login_manager.user_loader(User.get)
-    login_manager.login_view = "/login"
+    _init_db(app, create_db)
+    _init_jinja(app)
+    _init_login(app)
 
     app.register_blueprint(module)
     return app
