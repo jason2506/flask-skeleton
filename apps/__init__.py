bb952ed97f571e0008149fe1d4f6e30b68ae2c59
Chi-En Wu <jason2506.mail@gmail.com>
2015-03-25 11:07:41 +0800

    Use Flask-Assets to merge and compress JavaScript and CSS files

--- apps/__init__.py
+++ apps/__init__.py
@@ -2,6 +2,7 @@
 
 from flask import Flask
 from flask.ext.login import LoginManager
+from flask.ext.assets import Environment, Bundle
 
 from .models import db, User
 from .views import module
@@ -10,15 +11,33 @@ __all__ = ('create_app',)
 
 
 def _init_db(app):
+    """Setup for Flask-SQLAlchemy."""
     db.app = app
     db.init_app(app)
 
 
+def _init_assets(app):
+    """Setup for Flask-Assets."""
+    scripts = Bundle('js/script.js',
+                     filters='closure_js',
+                     output='js/scripts.%(version)s.js')
+    styles = Bundle('css/style.css',
+                    filters='yui_css',
+                    output='css/styles.%(version)s.css')
+
+    env = Environment(app)
+    # env.url_expire = True
+    env.register('scripts', scripts)
+    env.register('styles', styles)
+
+
 def _init_jinja(app):
+    """Setup for Jinja2."""
     pass
 
 
 def _init_login(app):
+    """Setup for Flask-Login."""
     login_manager = LoginManager()
     login_manager.init_app(app)
     login_manager.user_loader(User.get)
@@ -26,6 +45,7 @@ def _init_login(app):
 
 
 def create_app(name=None):
+    """Create and initialize app."""
     if name is None:
         name = __name__
 
@@ -33,6 +53,7 @@ def create_app(name=None):
     app.config.from_object('config')
 
     _init_db(app)
+    _init_assets(app)
     _init_jinja(app)
     _init_login(app)
 
